d0c534a6dd3957bf6155d22f610b839d49be652b
Kcodev <odkhoder@gmail.com>
2023-05-09 16:36:53 +0300

    Logic

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username':'Sammy'}
+    posts = [
+
+        {
+            'author':{'username':'John'},
+            'body':'Beautiful day in Portland'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'The averagers movie was cool!'
+        }
+    ]
+    return render_template('index.html',title = 'Home', user=user, posts=posts)
